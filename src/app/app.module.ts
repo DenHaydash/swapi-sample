665e418654bfa81a56f93ad712797e6f8b94557b
Denis Haidash <danhaydash@gmail.com>
2018-10-19 14:26:39 +0300

    Use absolute paths in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/app/app.component';
-import { PlanetsListComponent } from './components/planets-list/planets-list.component';
-import { PlanetInfoComponent } from './components/planet-info/planet-info.component';
-import { PersonInfoComponent } from './components/person-info/person-info.component';
-import { PeopleListComponent } from './components/people-list/people-list.component';
-import { MoviesDropdownComponent } from './components/movies-dropdown/movies-dropdown.component';
+import { AppRoutingModule } from 'app/app-routing.module';
+import { AppComponent } from 'app/components/app/app.component';
+import { PlanetsListComponent } from 'app/components/planets-list/planets-list.component';
+import { PlanetInfoComponent } from 'app/components/planet-info/planet-info.component';
+import { PersonInfoComponent } from 'app/components/person-info/person-info.component';
+import { PeopleListComponent } from 'app/components/people-list/people-list.component';
+import { MoviesDropdownComponent } from 'app/components/movies-dropdown/movies-dropdown.component';
 
 @NgModule({
   declarations: [
