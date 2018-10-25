3648ce0642430c8b3f0dce4a82a1dacfb0e59e1f
Denis Haidash <danhaydash@gmail.com>
2018-10-25 17:36:07 +0300

    Small fixes

--- src/app/services/swapi-client.service.ts
+++ src/app/services/swapi-client.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map, share } from 'rxjs/operators';
 
-import { environment } from 'app/../environments/environment';
+import { environment } from 'environments/environment';
 
 @Injectable({
   providedIn: 'root'
